08880bbd96c03eaa329baf0d0cb1083d9247d1a2
keinzuneadasi <keinzuneadasi@gmail.com>
2022-02-07 10:35:57 +0100

    updated code, whether e go run

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -1,33 +1,28 @@
-package com.example.splash
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
-        import android.content.Intent
-                import androidx.appcompat.app.AppCompatActivity
-                import android.os.Bundle
-                import android.os.Handler
-                import android.view.animation.AnimationUtils
-                import android.widget.ImageView
-        import com.example.testapplication.MainActivity
-        import com.example.testapplication.R
-        import com.example.testapplication.TESTActivity2
-        import java.util.jar.Manifest
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
 
-                @Suppress("DEPRECATION")
-                class SplashScreen : AppCompatActivity() {
-                    override fun onCreate(savedInstanceState: Bundle?) {
-                        super.onCreate(savedInstanceState)
-                        setContentView(R.layout.activity_main)
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-                        val backgroundImage:ImageView = findViewById(R.id.imagesplash)
-                        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
-                        backgroundImage.startAnimation(sideAnimation)
-
-
-                        Handler().postDelayed({
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }, 10000) // delaying for 4 seconds...
-
-
-                    }
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
 }
